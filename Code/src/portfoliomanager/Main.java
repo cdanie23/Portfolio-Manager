acf2e7a2e64b67a42a5c5aa2996e6e85fc034695
Mughki <bluntsam23@hotmail.com>
2025-02-15 23:56:34 -0500

    Implement Account Class
    Implemented Account model class for account creation with basic unit-testing.

--- Code/src/portfoliomanager/Main.java
+++ Code/src/portfoliomanager/Main.java
@@ -6,8 +6,6 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
 
-
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
