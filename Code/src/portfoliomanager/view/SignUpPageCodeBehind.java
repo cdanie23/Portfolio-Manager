6f907f08dd5d616e6e081063b8f919a7e9cc31e8
Mughki <bluntsam23@hotmail.com>
2025-02-24 12:21:31 -0500

    Implement Sign Up Functionality
    Implemented some functionality for the Sign Up page. Server implementation is needed to save account information. Password encryption is not currently implemented.

--- Code/src/portfoliomanager/view/SignUpPageCodeBehind.java
+++ Code/src/portfoliomanager/view/SignUpPageCodeBehind.java
@@ -5,21 +5,33 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import portfoliomanager.model.Account;
+
 /**
- * The sign up page code behind 
+ * The sign up page code behind
+ * 
  * @author Colby
+ * @author Sam
  * @version Spring 2025
  */
 public class SignUpPageCodeBehind  implements Initializable {
-
     @FXML
     private ImageView logoImageView;
-
     @FXML
     private Label signUpLabel;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private Button signUpButton;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -27,6 +39,29 @@ public class SignUpPageCodeBehind  implements Initializable {
         this.logoImageView.setImage(image);
 	}
     
+    @FXML
+    private void handleSignUp() {
+    	if (this.emailField.getText().isEmpty() || this.passwordField.getText().isEmpty()) {
+    		this.showAlert("Error", "Email or password are empty.");
+    		
+    		return;
+    	}
+    	
+    	String email = this.emailField.getText();
+    	String password = this.passwordField.getText();
+    	
+    	Account account = new Account(email, password);
+    	this.showAlert("Signed Up!", "Account email: " + email);
+    	
+    	Stage stage = (Stage) this.signUpButton.getScene().getWindow();
+        stage.close();
+    }
     
-    
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
---
+++ Code/src/portfoliomanager/viewmodel/SignUpPageViewModel.java
@@ -0,0 +1,11 @@
+package portfoliomanager.viewmodel;
+
+/**
+ * The Sign Up Page View Model
+ * 
+ * @author Sam
+ * @version Spring 2025
+ */
+public class SignUpPageViewModel {
+	// Server Logic Using ZMQ For Sign Up Page
+}
