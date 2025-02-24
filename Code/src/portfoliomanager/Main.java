ebc64ee3eef621111df9977a6d4f497b9ff23505
Adhika39 <165107869+Adhika39@users.noreply.github.com>
2025-02-24 17:56:35 -0500

    Implemented and test DataReader
    Implements and tests data reader to read crypto data in. Only one type of crypto.

--- Code/src/portfoliomanager/Main.java
+++ Code/src/portfoliomanager/Main.java
@@ -3,6 +3,7 @@ package portfoliomanager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import portfoliomanager.datareader.DataReader;
 import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
 
@@ -14,6 +15,7 @@ import javafx.scene.control.TabPane;
  */
 
 public class Main extends Application {
+	private static final String FILEPATH = "resources/BTC-USD_data.txt";
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -35,6 +37,9 @@ public class Main extends Application {
 	 * @param args command line arguments
 	 */
 	public static void main(String[] args) {
+		//Reads the data in
+		DataReader reader  = new DataReader(FILEPATH);
+		reader.readCryptoData();
 		launch(args);
 	}
 }
---
+++ Code/src/portfoliomanager/test/TestDataReader.java
@@ -0,0 +1,95 @@
+package portfoliomanager.test;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import portfoliomanager.datareader.DataReader;
+import portfoliomanager.model.CryptoCollection;
+
+class TestDataReader {
+	private File tempFile;
+	
+	@BeforeEach
+	void setUp(@TempDir Path tempDir) throws IOException {
+		this.tempFile = tempDir.resolve("BTC-USD_data.txt").toFile();
+	}
+	
+	@Test
+	void testReadCryptoData(@TempDir Path tempDir) throws IOException {
+        try (FileWriter writer = new FileWriter(this.tempFile)) {
+            writer.write("Date,Price\n"); 
+            writer.write("2024-01-01,50000\n");
+            writer.write("2024-01-02,51000\n");
+        }
+        DataReader dataReader = new DataReader(tempFile.getAbsolutePath());
+        dataReader.readCryptoData();
+        CryptoCollection collection = dataReader.getCryptoCollection();
+        assertAll(()-> assertFalse(collection.getCryptos().isEmpty()),
+        		()-> assertEquals(50000.0, collection.getCryptos().get(0).getCurrentPrice()));
+        
+	}
+	
+	@Test
+	void testFileNotFound() {
+		assertThrows(IllegalArgumentException.class, ()-> {
+			new DataReader("testFile.txt");
+		});
+	}
+	
+	@Test
+	void testNullFile() {
+		assertThrows(IllegalArgumentException.class, ()-> new DataReader(null));
+	}
+	
+	@Test
+	void testBlankFileName() {
+		assertThrows(IllegalArgumentException.class, ()-> new DataReader("  "));
+	}
+	
+	@Test
+	void testBlankFile() throws IOException {
+		try (FileWriter writer = new FileWriter(this.tempFile)) {
+            writer.write("Date,Price\n");
+        }
+		DataReader dataReader = new DataReader(tempFile.getAbsolutePath());
+        assertThrows(NoSuchElementException.class, ()-> {
+        	dataReader.readCryptoData();
+        });
+	}
+	
+	@Test
+    void testReadInvalidPrice() throws IOException {
+        try (FileWriter writer = new FileWriter(tempFile)) {
+            writer.write("Date,Price\n");
+            writer.write("2024-01-01,50000\n");
+            writer.write("2024-01-02,INVALID_PRICE\n");
+        }
+
+        DataReader reader = new DataReader(tempFile.getAbsolutePath());
+
+        assertThrows(NumberFormatException.class, () -> reader.readCryptoData());
+    }
+	
+	@Test
+	void testFileContainsLessColumn() throws IOException {
+        try (FileWriter writer = new FileWriter(tempFile)) {
+            writer.write("Date,Price\n");
+            writer.write("2024-01-01,50000\n");
+            writer.write("2024-01-02,\n");
+        }
+        DataReader reader = new DataReader(tempFile.getAbsolutePath());
+
+        assertThrows(IllegalArgumentException.class, () -> reader.readCryptoData());
+	}
+	
+ }
